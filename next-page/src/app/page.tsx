5a120a2e5a7539b130d17126425d1b91cd5ef2b9
Jun Guo <gjuoun@gmail.com>
2023-03-16 22:56:58 -0300

    should work

--- next-page/src/app/page.tsx
+++ next-page/src/app/page.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
